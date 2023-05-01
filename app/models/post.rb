65da2f57d271c47459a91f1da44d5866541b0828
Pauline Muigai <wanjirumuigai@gmail.com>
2023-05-01 11:28:28 +0300

    completed lab

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w[Fiction Non-Fiction] }
+
+  validate :clickbait
+
+  def clickbait
+    clickbait_titles = [/Won't Believe/i, /Secret/i, /Top \d/i, /Guess/i]
+
+    if clickbait_titles.none? { |clickbait| clickbait.match title }
+      errors.add(:title, "Your title must be more clickbait-y")
+    end
+  end
 end
